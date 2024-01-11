ad0108dcc379f483ebd46fdb63dcbeed4c73ac66
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-01-12 02:26:46 +0530

    use of axios to fetch data from backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import products from "../products";
 import Product from "../components/Product";
+import axios from "axios";
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getProducts();
+  }, []);
+  const getProducts = async () => {
+    try {
+      const { data } = await axios.get("http://127.0.0.1:8000/api/products/");
+      setProducts(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="homeContainer">
       <h1>Latest Products</h1>
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import products from "../products";
 import { Link, useParams } from "react-router-dom";
 import Rating from "../components/Rating";
+import axios from "axios";
 
 const ProductPage = () => {
   const { id } = useParams();
   const productId = id;
-  const product = products.find((p) => p._id === productId);
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+  const getProduct = async () => {
+    try {
+      const { data } = await axios.get(
+        `http://127.0.0.1:8000/api/products/${productId}`
+      );
+      setProduct(data);
+      console.log(product);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="productPageContainer">
       <Link to="/">
