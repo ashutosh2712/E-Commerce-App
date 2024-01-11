fde47c30a06f44010a863d0ad3e8eaace41ed766
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-01-12 00:52:47 +0530

    initial setups and first view.

---
+++ base/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .products import products
+
+
+# Create your views here.
+@api_view(["GET"])
+def getRoutes(request):
+    return Response("Welcome to E-commerce BackEnd")
+
+
+@api_view(["GET"])
+def getProducts(request):
+    return Response(products)
