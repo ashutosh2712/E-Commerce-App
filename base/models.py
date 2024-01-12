d4fbbe11fca62b80a62477f7673bdd8672c2360f
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-01-12 21:20:42 +0530

    static images setups with installation of pillow library.

--- base/models.py
+++ base/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    image = models.CharField(max_length=200, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
@@ -18,7 +18,7 @@ class Product(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
-        return self(self.name)
+        return str(self.name)
 
 
 class Review(models.Model):
