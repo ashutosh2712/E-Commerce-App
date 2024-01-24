8ab0b81978fcad99c9460066edc72655f80fb6aa
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-01-25 00:01:45 +0530

    Register user done.

--- base/views.py
+++ base/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Product
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
-
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.shortcuts import render
+from rest_framework import status
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -28,6 +30,23 @@ class MyTokenObtainPairView(TokenObtainPairView):
 @api_view(["GET"])
 def getRoutes(request):
     return Response("Welcome to E-commerce BackEnd")
+
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except IntegrityError:
+        return Response({'error: User already Exist!'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
