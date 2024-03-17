dd480a407bc295907c8e0bef746a70c64339d9b8
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-03-17 22:52:42 +0530

    getting top products view done.

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
      path("upload", views.uploadImage, name="image-upload"),
      
      path("<str:pk>/reviews", views.createProductReview, name="create-review"),
+     path("top", views.getTopproducts, name="top-products" ),
     path("<str:pk>", views.getProduct, name="product"),
     path("update/<str:pk>", views.updateProduct, name="product-update"),
     path("delete/<str:pk>", views.deleteProduct, name="product-delete"),
--- base/views/product_views.py
+++ base/views/product_views.py
@@ -17,7 +17,15 @@ def getProducts(request):
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
-
+@api_view(["GET"])
+def  getTopproducts(request) :
+     products = Product.objects.filter(rating__gte=4).order_by('-rating')[0:5]
+     
+     serializer = ProductSerializer(products, many=True)
+     
+     return Response(serializer.data)
+     
+    
 @api_view(["GET"])
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
