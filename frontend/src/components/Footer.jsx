569cb2ceb25c75f2257532e571f26f6c3f45f3bf
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-03-31 02:32:27 +0530

    npm build refactor

--- frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-  return <div className="footerContainer">copyright &copy; ClickShop</div>;
-};
-
-export default Footer;
--- frontend/src/components/Message.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Message = ({ className, children }) => {
-  return <div className={`messageContainer ${className}`}>{children}</div>;
-};
-
-export default Message;
